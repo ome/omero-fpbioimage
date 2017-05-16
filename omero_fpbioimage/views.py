ec874bd35a93adb489573631d9a14a32832c9b8a
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-05-16 22:40:27 +0100

    Adjust query to check physical size and resize the image

--- omero_fpbioimage/views.py
+++ omero_fpbioimage/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from omeroweb.webclient.decorators import login_required
 
+import PIL
 from PIL import Image
 from cStringIO import StringIO
 
@@ -15,9 +16,31 @@ def index(request):
 @login_required()
 def fpbioimage(request, image_id, conn=None, **kwargs):
     """Load the viewer page for the image."""
-    image = conn.getObject('image', image_id)
-    context = {'image': image}
+    image = conn.getObject('Image', image_id)
+    px = image.getPrimaryPixels().getPhysicalSizeX()
+    x = 1
+    y = 1
+    z = 1
 
+    if (px is not None):
+        size = image.getPixelSizeX(True)
+        x = size.getValue()
+
+    py = image.getPrimaryPixels().getPhysicalSizeY()
+    if (py is not None):
+        size = image.getPixelSizeZ(True)
+        y = size.getValue()
+
+    pz = image.getPrimaryPixels().getPhysicalSizeZ()
+    if (pz is not None):
+        size = image.getPixelSizeZ(True)
+        z = size.getValue()
+
+    context = {'image': image,
+               'size_x': x,
+               'size_y': y,
+               'size_z': z
+               }
     return render(request, 'fpbioimage/viewer.html', context)
 
 
@@ -26,8 +49,13 @@ def fpbioimage_png(request, image_id, the_z, conn=None, **kwargs):
     """Render png for image at specified Z section."""
     image = conn.getObject('image', image_id)
     jpeg_data = image.renderJpeg(the_z, 0, compression=0.9)
-
+    width = max(image.getSizeX, image.getSizeY)
+    if max > 500:
+        width = 500
     i = Image.open(StringIO(jpeg_data))
+    p = (width/float(i.size[0]))
+    hsize = int((float(i.size[1])*float(p)))
+    i = i.resize((width, hsize), PIL.Image.ANTIALIAS)
     output = StringIO()
     i.save(output, 'png')
     png_data = output.getvalue()
