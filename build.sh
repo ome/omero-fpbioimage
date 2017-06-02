60b75b0c42c2b25ec91dcc0edfb2d05d81e1d262
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-06-02 22:40:37 +0100

    Install from master or install released version

--- build.sh
+++ build.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+RELEASE=${1:-true}
 
 DIR=__MACOSX
-echo "Retrieving latest FPBioimage"
-curl -s -L https://github.com/fpBioImage/fpBioImage.github.io/releases/latest | egrep -o '/fpBioImage/fpBioImage.github.io/releases/download/[v]?[0-9.]*/FPBioimage.zip' | wget --base=http://github.com/ -i - -O FPBioimage.zip
-unzip -q FPBioimage.zip
-rm FPBioimage.zip
-# clean if we want to replace version
-echo "Installing FPBioimage"
+echo "Deleting existing folder"
 rm -rf omero_fpbioimage/static/fpbioimage/FPBioimage
-mv FPBioimage/FPBioimage omero_fpbioimage/static/fpbioimage
-rm -rf FPBioimage
+if [ $RELEASE = "true" ]; then
+	echo "Retrieving latest release version of FPBioimage"
+	curl -s -L https://github.com/fpBioImage/fpBioImage.github.io/releases/latest | egrep -o '/fpBioImage/fpBioImage.github.io/releases/download/[v]?[0-9.]*/FPBioimage.zip' | wget --base=http://github.com/ -i - -O FPBioimage.zip
+	unzip -q FPBioimage.zip
+	rm FPBioimage.zip
+	echo "Installing FPBioimage"
+	mv FPBioimage/FPBioimage omero_fpbioimage/static/fpbioimage
+	rm -rf FPBioimage
+else
+	echo "Retrieving FPBioimage from master"
+	curl -o FPBioimage.zip -L https://github.com/fpBioImage/fpBioImage.github.io/archive/master.zip
+	unzip -q FPBioimage.zip
+	rm FPBioimage.zip
+	mv fpBioImage.github.io-master/dev/FPBioimage omero_fpbioimage/static/fpbioimage
+	rm -rf fpBioImage.github.io-master
+fi
+
 if [ -d "$DIR" ]; then
 	rm -rf $DIR
 fi
