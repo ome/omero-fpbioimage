e1dd5a01b9eb3b1b9496ab6df4840c9af514296b
William Moore <w.moore@dundee.ac.uk>
2023-05-12 16:41:45 +0100

    use re_path in urls.py

--- omero_fpbioimage/urls.py
+++ omero_fpbioimage/urls.py
@@ -22,7 +22,7 @@
 # Version: 1.0
 #
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
@@ -30,20 +30,20 @@ from . import views
 urlpatterns = [
 
     # index placeholder
-    url(r'^$', views.index, name='fpbioimage_index'),
+    re_path(r'^$', views.index, name='fpbioimage_index'),
 
     # Viewer page
-    url(r'^viewer/(?P<image_id>[0-9]+)/', views.fpbioimage,
+    re_path(r'^viewer/(?P<image_id>[0-9]+)/', views.fpbioimage,
         name='fpbioimage_viewer'),
 
     # PNG plane. This is used for 'first_image'
-    url(r'^imageStacks/(?P<image_id>[0-9]+)/(?P<atlas_index>[0-9]+)\.png',
+    re_path(r'^imageStacks/(?P<image_id>[0-9]+)/(?P<atlas_index>[0-9]+)\.png',
         views.fpbioimage_png, name='fpbioimage_png'),
 
     # PNG planes to load whole stack.
     # Can't seem to control how this url is generated in JavaScript from
     # the 'first_image' url above. So it's ugly but this works for now.
-    url(r'^viewer//fpbioimage/imageStacks/'
+    re_path(r'^viewer//fpbioimage/imageStacks/'
         '(?P<image_id>[0-9]+)/(?P<atlas_index>[0-9]+)\.png',
         views.fpbioimage_png, name='fpbioimage_png2'),
 ]
